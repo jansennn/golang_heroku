8d6faf66011be0a9dddee079c8bb92b63ca40b59
Andreas  Tampubolon <38531347+jansennn@users.noreply.github.com>
2021-10-20 16:11:20 +0700

    add endpoint project

--- entity/description.entity.go
+++ entity/description.entity.go
@@ -2,5 +2,5 @@ package entity
 
 type Description struct {
 	ID            int64  `gorm:"primary_key:auto_increment" json:"-"`
-	Description   string `gorm:"type:varchar(100)" json:"-"`
+	Description   string `gorm:"type:text" json:"-"`
 }
